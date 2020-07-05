006c3590dd4d0ea8d73150086ea80567514ac2d6
Brandon <brandonskerritt51@gmail.com>
2020-07-05 22:39:38 +0100

    Created GitHub Action Pytest & Initial class Library

---
+++ cogs/library.py
@@ -0,0 +1,15 @@
+# Initial file
+
+
+class library:
+
+    """The Library Class for TryHackMe
+
+    Allows access to a database to create a virtual library
+
+    Interfaces with a MySQL database
+
+    Allows the use of adding books, or getting top books by category"""
+
+    def __init__(self):
+        """TODO: to be defined. """
